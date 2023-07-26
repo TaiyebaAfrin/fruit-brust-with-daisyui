78b5ca9af9ce8f5c09edaf9c5aebfac8d47ec93c
Taiyebaafrin <sadiataiyeba7.com>
2023-07-27 04:46:12 +0600

    commit four

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+
+  module.exports = {
+    theme: {
+      screens: {
+        'sm': {'min': '400px', 'max': '767px'},
+        // => @media (min-width: 400px and max-width: 767px) { ... }
+  
+        'md': {'min': '768px', 'max': '970px'},
+        // => @media (min-width: 768px and max-width: 970px) { ... }
+  
+        'lg': {'min': '971px', 'max': '1279px'},
+        // => @media (min-width: 971px and max-width: 1279px) { ... }
+  
+        'xl': {'min': '1280px', 'max': '1535px'},
+        // => @media (min-width: 1280px and max-width: 1535px) { ... }
+  
+        '2xl': {'min': '1536px'},
+        // => @media (min-width: 1536px) { ... }
+      },
+    }
+  }
